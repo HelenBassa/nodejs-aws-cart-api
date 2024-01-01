c5242a933566fa088518dd466aede1432bc6593c
HelenBassa <helenbassa@gmail.com>
2024-01-02 01:21:25 +0300

    fix: add host

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     origin: (req, callback) => callback(null, true),
   });
   app.use(helmet());
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap().then(() => {
   console.log('App is running on %s port', port);
